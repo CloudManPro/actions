e37de15d6ac28ed2ba602e2c25428205aa59ccc4
CloudManPro <cloudmanpro111@gmail.com>
2025-12-19 13:32:35 -0300

    Update main.tf

--- testVpc/main.tf
+++ testVpc/main.tf
@@ -1,26 +1,13 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
     bucket = "s3-cloudman1-backend-test"
-    # A 'key' será injetada via linha de comando no GitHub Action
     region = "us-east-1"
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
+provider "aws" { region = "us-east-1" }
 
 resource "aws_vpc" "main" {
   cidr_block = "10.1.0.0/16"
-
-  tags = {
-    Name = "test-action"
-  }
+  tags = { Name = "main-vpc" }
 }
